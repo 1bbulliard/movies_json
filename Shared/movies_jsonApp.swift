10c447451cd57960e3d76fb35d095536dfb68279
bbulliard <bbulliard@gmail.com>
2021-11-02 14:06:58 -0500

    Initial Commit

---
+++ Shared/movies_jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_jsonApp.swift
+//  Shared
+//
+//  Created by Bob Bulliard on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct movies_jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
